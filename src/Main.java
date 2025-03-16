aa81b4bec0d37aa099ed6af6e088fa0a6d941f0f
Javad <javadnazirli24@gmail.com>
2025-03-17 01:12:34 +0400

    MySqlBasedDaoImpl.java was added. docker-compose.yaml was fixed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import main.dao.BookDAO;
-import main.dao.file.PostgresSqlBasedDaoImpl;
+import main.dao.db.MySqlBasedDaoImpl;
+import main.dao.db.PostgresSqlBasedDaoImpl;
 import main.model.BookEntity;
 
 public class Main {
     public static void main(String[] args) {
-        PostgresSqlBasedDaoImpl postgresSqlBasedDao = new PostgresSqlBasedDaoImpl();
-        postgresSqlBasedDao.getAllBooks().forEach(System.out::println);
-
+        MySqlBasedDaoImpl myDao = new MySqlBasedDaoImpl();
+        BookEntity book = new BookEntity("Text", "Text0", 2025);
+        BookEntity book2 = new BookEntity("Text", "Text2", 2025);
+        BookEntity book3 = new BookEntity("Text", "Text3", 2025);
+        myDao.addBook(book);
+        myDao.addBook(book2);
+        myDao.addBook(book3);
 
     }
 }
